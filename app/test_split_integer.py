af58e56c66ba65d696b867e9666e3263a3d01677
Vitaliy Stepanyuk <vist98@gmail.com>
2026-03-06 19:09:10 +0200

    Solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    summary = sum(split_integer(6, 2))
-    assert summary == 6
+    assert sum(split_integer(6, 2)) == 6
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -15,10 +14,16 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    if len(set(split_integer(6, 2))) != 1:
-        assert split_integer(6, 2) == sorted(split_integer(6, 2))
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(3, 5)
-    assert result == [0, 0, 1, 1, 1]
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+
+
+def test_list_contains_exactly_number_of_parts_elements() -> None:
+    assert len(split_integer(3, 5)) == 5
+
+
+def test_difference_between_largest_and_smallest_no_more_1() -> None:
+    assert split_integer(2, 2)[0] - split_integer(2, 2)[-1] <= 1
